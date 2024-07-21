7f7c49feea37380d9606166d4c15610a86172924
David Ren <cs.davidren@gmail.com>
2024-07-20 19:05:56 -0700

    Initial steps

---
+++ tutorial/home.js
@@ -0,0 +1,76 @@
+console.log('hello');
+alert('yoo')
+
+// variables
+var b = 'smoothie';
+// console.log(b);
+
+var someNumber = 45;
+// console.log(someNumber);
+
+//document.getElementById('someText').innerHTML = 'Hey There';
+
+// var age = prompt('What is your age?');
+
+// document.getElementById('someText').innerHTML = age;
+
+// Numbers in JavaScript
+
+// var num1 = 10;
+// num1++; // num1 = num1 + 1
+// num1--; // num1 = num1 - 1
+
+// console.log(num1);
+
+//functions
+
+// function fun() {
+//     alert('this alert is from a function');
+// }
+
+// fun();
+
+// function greeting() {
+//     var name = prompt('What is your name? ');
+//     var result = 'Hello' + ' ' + name;
+//     console.log(result);
+// }
+
+// var name = prompt('What is your name? ');
+
+// function greeting(name) {
+//     var result = 'Hello' + ' ' + name;
+//     console.log(result)
+// }
+
+// var name 
+// function sumNumbers(num1, num2){
+//     var result = num1 + num2;
+//     console.log(result);
+// }
+
+// sumNumbers(12,123);
+// sumNumbers('hello','John');
+ 
+// while loops and for loops
+
+// var i = 0;
+
+// while (i < 100){
+//     i += 1;
+//     console.log(i);
+// }
+
+// for (let num = 0; num <= 100; num++) {
+//     console.log(num);
+// }
+
+
+
+let number = 14;
+let string = 'Macelyne';
+let object = {first: 'Jane', last: 'Doe'}
+let bool = false;
+let array = ['apple', 'tsla', 'spy']
+let random; // undefined
+let nothing = null // nothing
